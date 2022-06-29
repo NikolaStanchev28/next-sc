3c71e31b8f9f3139c945aedbf987f5b49ef8c2de
rbtt <105424133+NikolaStanchev28@users.noreply.github.com>
2022-06-29 19:23:22 +0300

    added colors & readme

--- generate-app.js
+++ generate-app.js
@@ -2,48 +2,54 @@
 const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+var { red, yellow, cyan } = require("colors/safe");
 
 if (process.argv.length < 3) {
-  console.log("You have to provide a name to your app.");
-  console.log("For example :");
-  console.log("    npx next-sc my-app");
+  console.log(red("You have to provide a name to your app."));
+  console.log(yellow("For example :"));
+  console.log(yellow("    npx next-sc my-app"));
   process.exit(1);
 }
 
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/NikolaStanchev28/vachev-sc-boilerplate.git";
+const gitRepo = "https://github.com/NikolaStanchev28/vachev-sc-boilerplate.git";
 
 try {
   fs.mkdirSync(projectPath);
 } catch (err) {
   if (err.code === "EEXIST") {
     console.log(
-      `The file ${projectName} already exist in the current directory, please give it another name.`
+      red(
+        `Folder ${yellow(
+          "'" + projectName + "'"
+        )} already exist in the current directory, please give it another name.`
+      )
     );
   } else {
-    console.log(error);
+    console.log(red(error));
   }
   process.exit(1);
 }
 
 async function main() {
   try {
-    console.log("Downloading files...");
-    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+    console.log(cyan("Downloading files.."));
+    execSync(`git clone --depth 1 ${gitRepo} ${projectPath}`);
 
     process.chdir(projectPath);
 
-    console.log("Installing dependencies...");
+    console.log(cyan("Installing dependencies.."));
     execSync("npm install");
 
-    console.log("Removing useless files");
+    console.log(cyan("Removing useless files.."));
     execSync("npx rimraf ./.git");
 
-    console.log("The installation is done, your app is ready to use !");
+    console.log(cyan("The installation is done, your app is ready to use !"));
+    console.log(cyan("Happy coding !"));
   } catch (error) {
-    console.log(error);
+    console.log(red(error));
   }
 }
 main();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# next-sc
+
+> A boilerplate installer for NextJS/Typescript/StyledComponents projects
+
+## Use directy via npx
+
+```sh
+npx next-sc my-app
+cd my-app
+npm run dev
+```
+
+## Install locally
+
+```sh
+npm install next-sc -g
+next-sc my-app
+cd my-app
+npm run dev
+```
