08f8e59b4c3cf56b21071c69b891964eb11ccd31
NikolaStanchev28 <105424133+NikolaStanchev28@users.noreply.github.com>
2023-03-31 21:42:18 +0300

    added --experimental parameter for nextjs13 version

--- generate-app.js
+++ generate-app.js
@@ -2,7 +2,7 @@
 const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-var { red, yellow, cyan } = require("colors/safe");
+var { red, yellow, cyan, gray, green } = require("colors/safe");
 
 if (process.argv.length < 3) {
   console.log(red("You have to provide a name to your app."));
@@ -12,6 +12,7 @@ if (process.argv.length < 3) {
 }
 
 const projectName = process.argv[2];
+const useNextjs13 = process.argv[3] === "--experimental";
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
 const gitRepo = "https://github.com/NikolaStanchev28/vachev-sc-boilerplate.git";
@@ -35,8 +36,17 @@ try {
 
 async function main() {
   try {
+    if (useNextjs13) {
+      console.log(
+        cyan(`Thanks for trying out the ${red(`experimental`)} version!`)
+      );
+    }
     console.log(cyan("Downloading files.."));
-    execSync(`git clone --depth 1 ${gitRepo} ${projectPath}`);
+    execSync(
+      `git clone ${
+        useNextjs13 ? `-b feature/update-to-nextjs-13 --single-branch` : ``
+      } ${gitRepo} ${projectPath}`
+    );
 
     process.chdir(projectPath);
 
@@ -47,9 +57,17 @@ async function main() {
     execSync("npx rimraf ./.git");
 
     console.log(cyan("The installation is done, your app is ready to use !"));
+    console.log(
+      cyan(
+        `Remember to run ${gray(
+          "npm run prepare"
+        )} after you initialize your git repository.`
+      )
+    );
     console.log(cyan("Happy coding !"));
   } catch (error) {
     console.log(red(error));
+    fs.rmSync(projectPath, { recursive: true, force: true });
   }
 }
 main();
